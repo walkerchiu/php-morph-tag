554f4b20ac6980ce2cef4e29cc13065a6d13b88c
walkerchiu <chenjen.chiou@gmail.com>
2021-04-24 12:48:59 +0800

    Feat (Module): Add loadOptions and loadOptionsSelected function
    1. Add loadOptions function to TagService.
    2. Add loadOptionsSelected function to TagService.
    3. Style TagTrait.

--- src/Models/Services/TagService.php
+++ src/Models/Services/TagService.php
@@ -26,4 +26,71 @@ class TagService
     {
         return $this->repository->listOption($host_type, $host_id, $code);
     }
+
+    /**
+     * @param Blog    $blog
+     * @param String  $code
+     * @param Boolean $transform
+     * @return Array
+     */
+    public function loadOptions($blog, $code = null, $transform = true)
+    {
+        if (empty($code))
+            $code = config('app.locale');
+
+        $result = [];
+        $tags = $blog->tags()->get();
+
+        foreach ($tags as $tag) {
+            if ($transform)
+                array_push($result, [
+                    'value'      => $tag->id,
+                    'identifier' => $tag->identifier,
+                    'label'      => $tag->findLang($code, 'name')
+                ]);
+            else
+                array_push($result, [
+                    'id'         => $tag->id,
+                    'identifier' => $tag->identifier,
+                    'name'       => $tag->findLang($code, 'name')
+                ]);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param Boolean $isOwner
+     * @param $record
+     * @param String  $code
+     * @param Boolean $transform
+     * @return Array
+     */
+    public function loadOptionsSelected($isOwner, $record, $code = null, $transform = true)
+    {
+        if (empty($code))
+            $code = config('app.locale');
+
+        $result = [];
+        $tags = $isOwner
+                    ? $record->tags()->get()
+                    : $record->tags(true)->get();
+
+        foreach ($tags as $tag) {
+            if ($transform)
+                array_push($result, [
+                    'value'      => $tag->id,
+                    'identifier' => $tag->identifier,
+                    'label'      => $tag->findLang($code, 'name')
+                ]);
+            else
+                array_push($result, [
+                    'id'         => $tag->id,
+                    'identifier' => $tag->identifier,
+                    'name'       => $tag->findLang($code, 'name')
+                ]);
+        }
+
+        return $result;
+    }
 }
